4352a7df2bddd1bed41a08d3690cd74e6f877f71
Mohamed Belgaied Hassine <belgaied2@hotmail.com>
2022-06-01 16:02:31 +0200

    Removed provider configurations to make module comp. with count

--- variables.tf
+++ variables.tf
@@ -1,26 +1,3 @@
-variable "aws_access_key" {
-    type = string
-    description = "AWS ACCESS KEY"
-}
-variable "aws_secret_key" {
-    type = string
-    description = "AWS SECRET KEY"
-}
-variable "aws_region" {
-    type = string
-    description = "AWS REGION"
-}
-variable "api_url" {
-    type = string
-    description = "Rancher API URL"
-    default = ""
-}
-variable "token_key" {
-    type = string
-    description = "Rancher API Token"
-    default = ""
-}
-
 variable "sg_name" {
     type = string
     description = "Security Group name for the K3s Nodes"
