212e957baa7ab7383c319dab24e2483962b07429
Mohamed Belgaied Hassine <belgaied2@hotmail.com>
2022-06-01 16:00:08 +0200

    removed provider versions from tf files

--- providers.tf
+++ providers.tf
@@ -9,16 +9,3 @@ provider "rancher2" {
     token_key = var.token_key
     insecure = true
 }
-
-terraform{
-  required_providers {
-    rancher2 = {
-        source  = "rancher/rancher2"
-        version = "1.23.0"
-    }
-    aws = {
-        source = "hashicorp/aws"
-        version = "4.16.0"
-    }
-  }
-}
